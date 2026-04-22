8e77e318aaea0b054eb351804398628c3775975d
Sergio Gil <sergio@wonderbrands.co>
2026-04-21 23:29:00 -0600

    [UPD]

--- __manifest__.py
+++ __manifest__.py
@@ -33,7 +33,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '18.0',
+    'version': '18.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
